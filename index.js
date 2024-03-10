c109356d194ced1c4f53185c18d2ad9e52f83c2e
Hubdar Ali Rind <159878422+rind-hubdar@users.noreply.github.com>
2024-03-10 13:03:53 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,38 @@
+//DATA TYPES
+//STRING
+var myName = "Hudar Ali Rind";
+console.log(myName);
+var quote = "\"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful.\"";
+var author = "- Albert Schweitzer once said,";
+console.log(author + quote);
+//NUMBERS
+var x = 4;
+var y = 5;
+var z = 6;
+console.log("if x = 4, y = 5 and z = 6 then find");
+console.log("(x + z) * y = ", (x + z) * y);
+console.log("(x - y) / z = ", (x - y) / z);
+console.log("(y + z) / x = ", (y + z) / x);
+console.log("(x / z) * y = ", (x / z) * y);
+console.log("(y % x) + z = ", (y % x) + z);
+console.log("(x - z) % y = ", (x - z) % y);
+var myAge = 18;
+var totalAge = myAge + 2;
+console.log("I am ".concat(totalAge, " years old."));
+//BOOLEAN
+var isGovernor = true;
+var isNotGovernor = false;
+console.log(isGovernor);
+console.log(isNotGovernor);
+//Undefined
+var color;
+console.log(color);
+//NULL
+var myColor = null;
+console.log(myColor);
+//BIGINT 
+var bigNumber = BigInt(1321461876);
+console.log(bigNumber);
+// SYMBOL
+var mySymbol = Symbol("hello!");
+console.log(mySymbol);
---
+++ index.ts
@@ -0,0 +1,72 @@
+//DATA TYPES
+
+//STRING
+
+let myName: string = "Hudar Ali Rind";
+console.log(myName);
+
+let quote: string = `"Success is not the key to happiness. Happiness is the key to success. If you love what you are doing, you will be successful."`
+let author: string = "- Albert Schweitzer once said,";
+
+
+console.log(author+quote);
+
+
+//NUMBERS
+
+let x: number = 4;
+let y: number = 5;
+let z: number = 6;
+
+console.log("if x = 4, y = 5 and z = 6 then find");
+console.log("(x + z) * y = ",(x + z) * y);
+console.log("(x - y) / z = ",(x - y) / z);
+console.log("(y + z) / x = ",(y + z) / x);
+console.log("(x / z) * y = ",(x / z) * y);
+console.log("(y % x) + z = ",(y % x) + z);
+console.log("(x - z) % y = ",(x - z) % y);
+
+
+let myAge: number = 18;
+let totalAge: number = myAge + 2;
+
+console.log(`I am ${totalAge} years old.`);
+
+
+//BOOLEAN
+
+let isGovernor: boolean = true;
+let isNotGovernor: boolean = false;
+
+console.log(isGovernor);
+console.log(isNotGovernor);
+
+//Undefined
+
+let color;
+
+console.log(color);
+
+//NULL
+
+let myColor: null = null;
+
+console.log(myColor);
+
+
+//BIGINT 
+
+let bigNumber: bigint = BigInt(1321461876);
+
+console.log(bigNumber);
+
+
+// SYMBOL
+
+let mySymbol: symbol = Symbol("hello!");
+
+
+console.log(mySymbol);
+
+
+//THAT'S AND I HOPE YOU FIND IT HELPFULL AND PLEASE REVIEW IT .
